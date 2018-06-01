0eb823e5b46e811768686f96035a5814434d01e4
panCalka <r.j.r.wojcik@gmail.com>
2018-06-01 11:19:52 +0200

    add repository and in memoryDB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10001, 'USD', 'PLN', 2, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10002, 'EUR', 'DOL', 1, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values (10003, 'EUR', 'PLN', 4, 0);
